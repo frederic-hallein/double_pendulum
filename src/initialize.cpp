a907a84b65e427aa9cc7ce3c57d4bb8e4d70c9f7
Frederic Hallein <frederic.halleiin@outlook.com>
2023-07-16 09:42:25 +0200

    edit init

--- src/initialize.cpp
+++ src/initialize.cpp
@@ -82,6 +82,14 @@ void initialize(bool& is_initialized, double& m_1, double& m_2, double& L_1, dou
             std::cin.ignore(1000,'\n');
         }
 
+        std::cout << "\nPersonal setup: \n" << std::endl;
+        std::cout << "m_1 = " << m_1 << std::endl;
+        std::cout << "m_2 = " << m_2 << std::endl;
+        std::cout << "L_1 = " << L_1 << std::endl;
+        std::cout << "L_2 = " << L_2 << std::endl;
+        std::cout << "ini_theta_1 = " << ini_theta_1 << std::endl;
+        std::cout << "ini_theta_2 = " << ini_theta_2 << std::endl;
+
         is_initialized = true;
     }
 
